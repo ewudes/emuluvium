2061fb7d2fe3c2a12819aa5edd5aca40e34a6cf8
ewudes <ewudes@gmail.com>
2023-08-02 16:32:30 +0300

    fix: routing

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Switch, Route, Router as BrowserRouter } from "react-router-dom";
-import Tetris from "../../games/tetris/tetris";
+import Tetris from "../../games/tetris/board";
 import browserHistory from '../../browser-history';
+import './app.css'
 
 const App = () => {
   return (
     <BrowserRouter history={browserHistory}>
       <Switch>
         <Route exact path="/">
-          <Tetris/>
+          <div className="t-parent">
+            <Tetris/>
+          </div>
         </Route>
       </Switch>
     </BrowserRouter>
