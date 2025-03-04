7b5351e1841269a6b146811eb1d0f32e0dd05f64
Ewudes <ewudes@gmail.com>
2025-03-04 18:27:39 +0300

    fix: home component

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -6,10 +6,10 @@ import './home.scss'
 const Home = () => {
   return (
     <main className="home__wrapper">
-      <h1 className="home__title">super game 112 in 1</h1>
+      <h1 className="home__title">Bit Nostalgia</h1>
       <ul className="home__list">
         <li className="home__item">
-          <Link to="/tetris" className="home__link">109.tetris</Link>
+          <Link to="/tetris" className="home__link">TetroGrid</Link>
         </li>
       </ul>
     </main>
