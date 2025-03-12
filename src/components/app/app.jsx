9f8a7a8798628d7d6b8054ebd6730acb1179a752
Ewudes <ewudes@gmail.com>
2025-03-12 21:25:05 +0300

    add: font style

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Switch, Route, Router as BrowserRouter } from "react-router-dom";
 import browserHistory from '../../browser-history';
+import '../../vendor/fonts/fonts.scss';
 
 import Home from "../../pages/home/home";
 import Tetrogrid from "../../games/tetrogrid/tetrogrid";
