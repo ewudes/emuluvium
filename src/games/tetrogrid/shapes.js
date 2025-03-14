9db8bfedf07c9212fbfe5094d212bb5651919641
Ewudes <ewudes@gmail.com>
2025-03-14 14:30:09 +0300

    fix: moved the shapes to a file

---
+++ src/games/tetrogrid/shapes.js
@@ -0,0 +1,65 @@
+export const SHAPES = [
+  {
+    shape: [
+      { x: 0, y: 0 }, { x: 1, y: 0 },
+      { x: 0, y: 1 }, { x: 1, y: 1 },
+    ],
+    width: 2,
+    height: 2,
+    color: "yellow",
+  },
+  {
+    shape: [
+      { x: 0, y: 0 }, { x: 0, y: 1 },
+      { x: 0, y: 2 }, { x: 0, y: 3 },
+    ],
+    width: 1,
+    height: 4,
+    color: "cyan",
+  },
+  {
+    shape: [
+      { x: 0, y: 0 }, { x: 0, y: 1 },
+      { x: 0, y: 2 }, { x: 1, y: 2 },
+    ],
+    width: 2,
+    height: 3,
+    color: "orange",
+  },
+  {
+    shape: [
+      { x: 1, y: 0 }, { x: 1, y: 1 },
+      { x: 1, y: 2 }, { x: 0, y: 2 },
+    ],
+    width: 2,
+    height: 3,
+    color: "blue",
+  },
+  {
+    shape: [
+      { x: 0, y: 0 }, { x: 1, y: 0 },
+      { x: 1, y: 1 }, { x: 2, y: 1 },
+    ],
+    width: 3,
+    height: 2,
+    color: "red",
+  },
+  {
+    shape: [
+      { x: 1, y: 0 }, { x: 2, y: 0 },
+      { x: 0, y: 1 }, { x: 1, y: 1 },
+    ],
+    width: 3,
+    height: 2,
+    color: "green",
+  },
+  {
+    shape: [
+      { x: 0, y: 0 }, { x: 1, y: 0 }, { x: 2, y: 0 },
+      { x: 1, y: 1 },
+    ],
+    width: 3,
+    height: 2,
+    color: "purple",
+  },
+];
