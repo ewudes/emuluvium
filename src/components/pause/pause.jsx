98c2b05c65afc0979777db5e6c50979c8b9b70fe
Ewudes <ewudes@gmail.com>
2025-04-08 16:36:18 +0300

    add: pause component

---
+++ src/components/pause/pause.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./pause.scss";
+
+const Pause = () => {
+  return (
+    <>
+      <div className="pause">
+        <h2>Пауза</h2>
+      </div>
+      <div className="pause__overlay"></div>  
+    </> 
+  );
+}
+
+export default Pause;
