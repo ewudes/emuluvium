c5350dccc87b49d2bc24aff61cb5d6bdcf8497c9
Ewudes <ewudes@gmail.com>
2025-04-10 18:35:27 +0300

    fix: hamburger menu

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -4,12 +4,12 @@ import "./nav.scss";
 const Nav = () => {
   return (
     <>
-      <div className="hamburger not-active">
-        <span className="hamburger__item"></span>
-        <span className="hamburger__item"></span>
-        <span className="hamburger__item"></span>
+      <div className="nav not-active">
+        <span className="nav__item"></span>
+        <span className="nav__item"></span>
+        <span className="nav__item"></span>
       </div>
-      <div className="tetrogrid__nav"></div>
+      <div className="nav__wrapper"></div>
     </> 
   );
 }
