3fa0f3a3a81375475a32ae0385eb89f3e3b961f7
Ewudes <ewudes@gmail.com>
2025-04-25 16:09:59 +0300

    add: game selection using the keyboard

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,39 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState, useRef } from 'react';
+import { Link, withRouter } from "react-router-dom";
 import { HINT_SCHEME } from "./const";
 import Nav from "../../components/nav/nav";
 import Hint from "../../components/hint/hint";
 
 import "./home.scss";
 
-const Home = () => {
+const Home = ({ history }) => {
+  const links = [
+    { label: 'TetroGrid', to: '/tetrogrid' },
+    { label: 'Snatrix', to: '/snatrix' },
+  ];
+
+  const [activeIndex, setActiveIndex] = useState(0);
+  const linkRefs = useRef([]);
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === 'ArrowDown') {
+        setActiveIndex((prev) => (prev + 1) % links.length);
+      } else if (e.key === 'ArrowUp') {
+        setActiveIndex((prev) => (prev - 1 + links.length) % links.length);
+      } else if (e.key === ' ' || e.key === 'Enter') {
+        e.preventDefault();
+        const activeLink = links[activeIndex];
+        if (activeLink) {
+          history.push(activeLink.to);
+        }
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, [activeIndex, links, history]);
+
   return (
     <main className="home__wrapper">
       <div className="home__bg-wrap">
@@ -21,12 +48,20 @@ const Home = () => {
       <div className="home__overlay">
         <h1 className="home__title">Emuluvium</h1>
         <ul className="home__list">
-          <li className="home__item">
-            <Link to="/tetrogrid" className="home__link">TetroGrid</Link>
-          </li>
-          <li className="home__item">
-            <Link to="/snatrix" className="home__link">Snatrix</Link>
-          </li>
+          {links.map((link, index) => (
+            <li
+              key={link.to}
+              className={`home__item ${index === activeIndex ? 'home__item-active' : ''}`}
+            >
+              <Link
+                to={link.to}
+                className="home__link"
+                ref={(el) => (linkRefs.current[index] = el)}
+              >
+                {link.label}
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
       <Hint scheme={HINT_SCHEME} mode={HINT_SCHEME.MODE}/>
@@ -34,4 +69,4 @@ const Home = () => {
   );
 }
 
-export default Home;
+export default withRouter(Home);
