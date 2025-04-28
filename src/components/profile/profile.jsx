799969cd0a370090f459b66eead7e8a2b280a8e9
Ewudes <ewudes@gmail.com>
2025-04-28 19:03:28 +0300

    fix: profile component

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -4,7 +4,18 @@ import "./profile.scss";
 const Profile = () => {
   return (
     <div className="profile">
-      <div className="profile__name">Лена Головач</div>
+      <div className="profile__name">Анонимный Котик</div>
+      <div className="profile__overlay">
+        <ul className="profile__list">
+          <li className="profile__item">Аватар</li>
+          <li className="profile__item">Анонимный Котик</li>
+          <li className="profile__item">Любимая игра: N/A</li>
+          <li className="profile__item">Всего очков: N/A</li>
+          <li className="profile__item">Всего время: N/A</li>
+          <li className="profile__item">Ранг: N/A</li>
+          <li className="profile__item">Найдено котиков: N/A</li>
+        </ul>
+      </div>
     </div>
   );
 }
