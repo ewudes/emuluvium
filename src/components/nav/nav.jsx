10389fcea02929d685a94354052a062373f923d9
Ewudes <ewudes@gmail.com>
2025-04-28 19:02:58 +0300

    fix: aside menu

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,13 +1,18 @@
 import React, { useState, useRef, useEffect } from "react";
+import Checkbox from "../checkbox/checkbox";
 import "./nav.scss";
 
-const Nav = () => {
+const Nav = ({ onActiveChange }) => {
   const [isActive, setIsActive] = useState(false);
   const navRef = useRef(null);
   const wrapperRef = useRef(null);
 
   const handleNavClick = () => {
-    setIsActive(prev => !prev);
+    setIsActive(prev => {
+      const newState = !prev;
+      onActiveChange(newState);
+      return newState;
+    });
   };
 
   useEffect(() => {
@@ -17,24 +22,42 @@ const Nav = () => {
 
       if (clickedOutsideNav && clickedOutsideWrapper) {
         setIsActive(false);
+        onActiveChange(false);
       }
     };
 
     document.addEventListener('mousedown', handleClickOutside);
     return () => document.removeEventListener('mousedown', handleClickOutside);
-  }, []);
+  }, [onActiveChange]);
 
   return (
     <>
       <div className={`nav ${isActive ? 'nav--active' : 'nav--not-active'}`} ref={navRef} onClick={handleNavClick}>
-        <span className="nav__item"></span>
-        <span className="nav__item"></span>
-        <span className="nav__item"></span>
+        <span className="nav__dash"></span>
+        <span className="nav__dash"></span>
+        <span className="nav__dash"></span>
       </div>
       <div
         className={`nav__wrapper ${isActive ? 'nav__wrapper-active' : ''}`}
         ref={wrapperRef}
-      ></div>
+      >
+        <div className="nav__buttons">
+          <button className="nav__button">Войти</button>
+          <button className="nav__button">Регистрация</button>
+        </div>
+        <ul className="nav__list">
+          <li className="nav__item">
+            <Checkbox label="Язык" id="lang"/>
+          </li>
+          <li className="nav__item">
+            <Checkbox label="Тема" id="scheme"/>
+          </li>
+          <li className="nav__item">
+            <Checkbox label="Звук" id="volume"/>
+          </li>
+        </ul>
+        <div className="nav__author">Made with 🖤 ewudes</div>
+      </div>
     </> 
   );
 }
