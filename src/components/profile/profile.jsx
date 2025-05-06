0f4574b17ac2f7b64d8f8c9ac98aeb7e72b17a29
Ewudes <ewudes@gmail.com>
2025-05-06 18:20:08 +0300

    fix: profile modal

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 import "./profile.scss";
+import LeaderPic from "./../../vendor/decorate/profile-avatar.png";
 
 const Profile = () => {
   const [isOverlayVisible, setIsOverlayVisible] = useState(false);
@@ -47,14 +48,22 @@ const Profile = () => {
           className={`profile__overlay profile__overlay--true`}
           ref={overlayRef}
         >
-          <ul className="profile__list">
-            <li className="profile__item">Аватар</li>
-            <li className="profile__item">Анонимный Котик</li>
-            <li className="profile__item">Любимая игра: N/A</li>
-            <li className="profile__item">Всего очков: N/A</li>
-            <li className="profile__item">Всего время: N/A</li>
-            <li className="profile__item">Ранг: N/A</li>
-            <li className="profile__item">Найдено котиков: N/A</li>
+          <div className="profile__pic">
+            <img src={LeaderPic} width="100" height="100" alt="Изображение профиля"/>
+          </div>
+          <div className="profile__username">Анонимный Котик</div>
+          <div className="profile__hint">Хочешь изменить профиль? <br/> Сначала <a href="#">войди</a> или <a href="#">зарегистрируйся</a>!</div>
+          <ul className="profile__awards-list">
+            {[...Array(10)].map((_, index) => (
+              <li className="profile__awards-item" key={index}>
+                <img
+                  src={require(`./../../vendor/decorate/awards/cat-${index + 1}-close.png`)}
+                  width={115}
+                  alt={`Награда котика ${index + 1}`}
+                  className="profile__awards-image"
+                />
+              </li>
+            ))}
           </ul>
         </div>
       )}
